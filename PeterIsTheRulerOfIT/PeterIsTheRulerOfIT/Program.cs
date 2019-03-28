36c73187ddc03413302e07f62076c33ddacde7de
petersigurdson <peter@petersigurdson.net>
2019-03-28 15:17:52 -0400

    I added Download() Method

--- PeterIsTheRulerOfIT/PeterIsTheRulerOfIT/Program.cs
+++ PeterIsTheRulerOfIT/PeterIsTheRulerOfIT/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PeterIsTheRulerOfIT
@@ -11,6 +12,14 @@ namespace PeterIsTheRulerOfIT
         static void Main(string[] args)
         {
             Console.WriteLine("Peter Rules the IT Universe!");
+            Download();
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("Download Complete"); 
         }
     }
 }
